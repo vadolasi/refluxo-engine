56d0348710f249b839625fe5f89c28a6190a09d7
Vitor Daniel <vitor036daniel@gmail.com>
2025-12-30 23:50:25 -0300

    chore: use lefthook instanced of husky

--- lint-staged.config.mjs
+++
@@ -1,7 +0,0 @@
-/**
- * @type {import("lint-staged").Configuration}
- */
-export default {
-  "*": "biome check --write --no-errors-on-unmatched --files-ignore-unknown=true",
-  "*.ts": "vitest related --run --passWithNoTests"
-}
